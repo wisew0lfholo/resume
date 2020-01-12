846118465ede0e4e37405dd1c4bf75fc228da6c5
Isaiah Nixon <isaiahnixon@gmail.com>
2020-01-11 23:55:23 -0700

    Updates from 1/11.
    Streamlined partials implementation + Updated resume information + Removed SASS cache + Updated print utility class.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 (function($) {
     // Load the partial components.
-    $('.summary').load('partials/summary.html');
-    $('#experience').load('partials/experience.html');
-    $('#projects').load('partials/projects.html');
-    $('.education').load('partials/education.html');
-    $('.languages').load('partials/languages.html');
-    $('.skills').load('partials/skills.html');
-    $('.tools').load('partials/tools.html');
-    $('.frameworks').load('partials/frameworks.html');
-    $('.badges').load('partials/badges.html');
+    $('*[partial]').each(function() {
+        $(this).load('partials/' + $(this).attr('partial') + '.html');
+    });
 })(jQuery);
